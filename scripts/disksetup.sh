9ca7a7c7b7856d4c40f218212c30cd98224e2123
riza <thomas.bemme@gmail.com>
2024-06-09 13:17:45 +0000

    Adding script for BTRFS creation

---
+++ scripts/disksetup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+DISK=/dev/sdb
+# Create partitions
+printf "label: gpt\n,550M,U\n,,L\n" | sfdisk "$DISK"
+# Format the EFI partition
+mkfs.vfat -n BOOT "$DISK"1
+
+cryptsetup --verify-passphrase -v luksFormat "$DISK"2
+cryptsetup open "$DISK"2 rootfs
+
+# Creat the swap inside the encrypted partition
+mkfs.btrfs /dev/mapper/rootfs
+
+# Then create subvolumes
+
+mount -t btrfs /dev/mapper/rootfs /mnt
+
+# We first create the subvolumes outlined above:
+btrfs subvolume create /mnt/root
+btrfs subvolume create /mnt/home
+btrfs subvolume create /mnt/nix
+
+umount /mnt
+
+# Mount the directories
+
+mount -o subvol=root,compress=zstd,noatime /dev/mapper/rootfs /mnt
+
+mkdir /mnt/{home,nix}
+mount -o subvol=home,compress=zstd,noatime /dev/mapper/rootfs /mnt/home
+mount -o subvol=nix,compress=zstd,noatime /dev/mapper/rootfs /mnt/nix
+
+# don't forget this!
+mkdir /mnt/boot
+mount "$DISK"1 /mnt/boot
+
+git clone https://git.kbnetcloud.de/user/nixos.git
