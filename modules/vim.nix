f03c1dc955bbe7105b8bb27990611bf0f1497801
Thomas Bemme <thomas.bemme@bs.nttdata.com>
2025-02-05 15:27:51 +0100

    Adding more Vim config

--- modules/vim.nix
+++ modules/vim.nix
@@ -6,7 +6,7 @@
       name = "vim";
       # Install plugins for example for syntax highlighting of nix files
       vimrcConfig.packages.myplugins = with pkgs.vimPlugins; {
-        start = [ vim-nix vim-lastplace dracula-vim ];
+        start = [ vim-nix vim-misc vim-lastplace supertab vim-signify vim-fugitive vim-airline dracula-vim ];
         opt = [ ];
       };
       vimrcConfig.customRC = ''
@@ -20,7 +20,9 @@
         set shiftwidth=2
         set tabstop=2
         set wildmenu
+        color dracula
         syntax on
+        hi Normal guibg=NONE ctermbg=NONE
       '';
     })
   ];
