2d9f8fd6a268b39bfcd69974fbea028cf14c4da6
Thomas Bemme <thomas.bemme@gmail.com>
2025-02-12 22:15:12 +0100

    Update Flake add shortcut for Ghostty

--- home/desktop.nix
+++ home/desktop.nix
@@ -191,5 +191,15 @@
     "org/gnome/mutter" = {
       dynamic-workspaces = true;
     };
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+      ];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      name = "Ghostty";
+      command = "ghostty";
+      binding = "<Super>Return";
+    };
   };
 }
