bdc7495f40c8bd3567bb29e21f3fed2be4ca93c7
Thomas Bemme <thomas.bemme@gmail.com>
2025-03-23 15:27:18 +0100

    Move default packages

--- modules/desktop-software.nix
+++ modules/desktop-software.nix
@@ -41,29 +41,8 @@
     ]) ++
     (with pkgs-unstable; [
       cryptomator
-      ghostty
       handbrake
     ]);
-  };
-
-  programs.firefox = {
-    enable = true;
-    package = pkgs-unstable.librewolf;
-    policies = {
-      DisableTelemetry = true;
-      DisableFirefoxStudies = true;
-      Preferences = {
-        "cookiebanners.service.mode.privateBrowsing" = 2; # Block cookie banners in private browsing
-        "cookiebanners.service.mode" = 2; # Block cookie banners
-        "privacy.donottrackheader.enabled" = true;
-        "privacy.fingerprintingProtection" = true;
-        "privacy.resistFingerprinting" = true;
-        "privacy.trackingprotection.emailtracking.enabled" = true;
-        "privacy.trackingprotection.enabled" = true;
-        "privacy.trackingprotection.fingerprinting.enabled" = true;
-        "privacy.trackingprotection.socialtracking.enabled" = true;
-      };
-    };
   };
 
   environment.systemPackages = with pkgs; [
--- modules/desktop.nix
+++ modules/desktop.nix
@@ -37,6 +37,32 @@
     algorithm = "zstd";
   };
 
+  users.users.${vars.user} = {
+    packages = with pkgs-unstable; [
+      ghostty
+    ];
+  };
+
+  programs.firefox = {
+    enable = true;
+    package = pkgs-unstable.librewolf;
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      Preferences = {
+        "cookiebanners.service.mode.privateBrowsing" = 2; # Block cookie banners in private browsing
+        "cookiebanners.service.mode" = 2; # Block cookie banners
+        "privacy.donottrackheader.enabled" = true;
+        "privacy.fingerprintingProtection" = true;
+        "privacy.resistFingerprinting" = true;
+        "privacy.trackingprotection.emailtracking.enabled" = true;
+        "privacy.trackingprotection.enabled" = true;
+        "privacy.trackingprotection.fingerprinting.enabled" = true;
+        "privacy.trackingprotection.socialtracking.enabled" = true;
+      };
+    };
+  };
+
   # List services that you want to enable:
   services.btrfs.autoScrub.enable = true;
   services.btrfs.autoScrub.interval = "weekly";
