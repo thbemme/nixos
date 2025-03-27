1b1df1edf29c003a4e3359eb4ac71a0c701a3454
Thomas Bemme <thomas.bemme@gmail.com>
2025-03-27 16:20:31 +0100

    Update Flake

--- hosts/puffy/hardware-configuration.nix
+++ hosts/puffy/hardware-configuration.nix
@@ -59,4 +59,6 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.amdgpu.initrd.enable = lib.mkDefault true;
+
 }
