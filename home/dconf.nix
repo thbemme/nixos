d8de7e607637b28fd4368043a95851a3777d41d8
Thomas Bemme <thomas.bemme@gmail.com>
2025-04-14 18:54:08 +0200

    Update Flake

--- home/dconf.nix
+++ home/dconf.nix
@@ -6,11 +6,11 @@
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
       font-name = "Inter Display 10";
-      document-font-name = "Inter 10";
+      document-font-name = "Inter Display 10";
       monospace-font-name = "Fira Code 10 @wght=400";
       font-hinting = "full";
       font-antialiasing = "rgba";
-      text-scaling-factor = "0.95";
+      text-scaling-factor = 1;
     };
   };
 }
