2a737e5d3c8c60f7cf13b04ac09b422710c0a11a
Thomas Bemme <thomas.bemme@gmail.com>
2025-05-13 22:57:34 +0200

    Reconfigure Librewolf and update Flake

--- home/desktop.nix
+++ home/desktop.nix
@@ -1,4 +1,4 @@
-_: {
+{pkgs, ...}: {
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through "home.file".
   home.file = {
@@ -17,5 +17,45 @@ _: {
 
   home.sessionVariables = {
     TERMINAL = "ghostty";
+  };
+
+  programs.firefox = {
+    enable = true;
+    package = pkgs.librewolf;
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      Preferences = {
+        "privacy.donottrackheader.enabled" = true;
+        "privacy.fingerprintingProtection" = true;
+        "privacy.resistFingerprinting" = false;
+        "privacy.trackingprotection.emailtracking.enabled" = true;
+        "privacy.trackingprotection.enabled" = true;
+        "privacy.trackingprotection.fingerprinting.enabled" = true;
+        "privacy.trackingprotection.socialtracking.enabled" = true;
+      };
+      ExtensionSettings = {
+        "addon@darkreader.org" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "vimium-c@gdh1995.cn" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-c/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "authenticator@mymindstorm" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/auth-helper/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "FirefoxColor@mozilla.com" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/firefox-color/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "idcac-pub@guus.ninja" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/istilldontcareaboutcookies/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
+    };
   };
 }
