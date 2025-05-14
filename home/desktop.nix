7c4d8d1f3290ae93577489a6437923a8a4055e1c
Thomas Bemme <thomas.bemme@gmail.com>
2025-05-14 18:39:45 +0200

    Merge main

--- home/desktop.nix
+++ home/desktop.nix
@@ -21,7 +21,7 @@
 
   programs.firefox = {
     enable = true;
-    package = pkgs.librewolf-bin;
+    package = pkgs.librewolf;
     policies = {
       DisableTelemetry = true;
       DisableFirefoxStudies = true;
