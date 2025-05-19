a4eb2e793114b1e089df62fd548d22b9d93fe203
Thomas Bemme <thomas.bemme@bs.nttdata.com>
2025-05-19 08:11:18 +0200

    Switch to nixos-25.05 and update Flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
