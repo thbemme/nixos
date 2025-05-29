d72f1ab1188daa1afb9c6ff51d2a704b9570b787
Thomas Bemme <thomas.bemme@gmail.com>
2025-05-29 15:05:36 +0200

    Reverting to old lm-sensors config

--- modules/amdgpu-patch/default.nix
+++ modules/amdgpu-patch/default.nix
@@ -10,7 +10,6 @@
     kernel = config.boot.kernelPackages.kernel;
   };
 in {
-
   # Workaround https://gitlab.freedesktop.org/drm/amd/-/issues?show=eyJpaWQiOiI0MjM4IiwiZnVsbF9wYXRoIjoiZHJtL2FtZCIsImlkIjoxMzMwODl9
   boot.extraModulePackages = [
     (amdgpu-kernel-module.overrideAttrs (_: {
--- modules/msib450mpro.nix
+++ modules/msib450mpro.nix
@@ -1,32 +1,25 @@
-{pkgs, ...}: {
-  environment.systemPackages = let
-    sensors_config = pkgs.writeTextDir "etc/sensors.d/msib450mpro.conf" ''
-      chip "nct6795-isa-*"
-        ignore fan1
-        ignore fan4
-        ignore fan5
-        ignore in10
-        ignore temp4
-        ignore temp5
-        ignore temp6
-        ignore temp8
-        ignore temp9
-        ignore temp10
-        ignore temp11
-        ignore temp12
-        label fan2 "CPU Fan"
-        ignore fan3 #"Case Fan"
-      chip "amdgpu-pci-*"
-        label fan1 "GPU Fan"
-    '';
-    sensors = pkgs.symlinkJoin {
-      inherit (pkgs.lm_sensors) name;
-      paths = [
-        pkgs.lm_sensors
-        sensors_config
-      ];
+_: {
+  config = {
+    environment.etc = {
+      "sensors3.conf".text = ''
+        chip "nct6795-isa-*"
+          ignore fan1
+          ignore fan4
+          ignore fan5
+          ignore in10
+          ignore temp4
+          ignore temp5
+          ignore temp6
+          ignore temp8
+          ignore temp9
+          ignore temp10
+          ignore temp11
+          ignore temp12
+          label fan2 "CPU Fan"
+          ignore fan3 #"Case Fan"
+        chip "amdgpu-pci-*"
+          label fan1 "GPU Fan"
+      '';
     };
-  in [
-    sensors
-  ];
+  };
 }
