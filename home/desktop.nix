bfab63f6f2c7d50291d958f2f8750d25a6b7e03a
Thomas Bemme <thomas.bemme@gmail.com>
2025-05-30 18:19:33 +0200

    Moving Librewolf

--- home/desktop.nix
+++ home/desktop.nix
@@ -6,45 +6,4 @@
   home.sessionVariables = {
     TERMINAL = "ghostty";
   };
-
-  programs.firefox = {
-    enable = true;
-    package = pkgs.librewolf;
-    policies = {
-      DisableTelemetry = true;
-      DisableFirefoxStudies = true;
-      Preferences = {
-        "privacy.donottrackheader.enabled" = true;
-        "privacy.fingerprintingProtection" = true;
-        "privacy.resistFingerprinting" = false;
-        "privacy.trackingprotection.emailtracking.enabled" = true;
-        "privacy.trackingprotection.enabled" = true;
-        "privacy.trackingprotection.fingerprinting.enabled" = true;
-        "privacy.trackingprotection.socialtracking.enabled" = true;
-      };
-      ExtensionSettings = {
-        installation_mode = "force_installed";
-        "addon@darkreader.org" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "vimium-c@gdh1995.cn" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-c/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "authenticator@mymindstorm" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/auth-helper/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "FirefoxColor@mozilla.com" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/firefox-color/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "idcac-pub@guus.ninja" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/istilldontcareaboutcookies/latest.xpi";
-          installation_mode = "force_installed";
-        };
-      };
-    };
-  };
 }
