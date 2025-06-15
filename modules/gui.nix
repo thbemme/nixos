3a64eb2fef09c992f7a8d2ef0fb5554e8f7889cd
Thomas Bemme <thomas.bemme@gmail.com>
2025-06-15 19:32:08 +0200

    Update Flake, fonts and sensors

--- modules/gui.nix
+++ modules/gui.nix
@@ -40,11 +40,23 @@
     ];
   };
 
-  fonts.packages = with pkgs; [
-    adwaita-fonts
-    fira-code
-    vistafonts
-  ];
+  fonts = {
+    enableDefaultPackages = true;
+    fontconfig = {
+      enable = true;
+      defaultFonts.monospace = ["Fira Code"];
+      defaultFonts.sansSerif = ["Adwaita Sans"];
+      defaultFonts.serif = ["Adwaita Serif"];
+      defaultFonts.emoji = ["Noto Color Emoji"];
+    };
+    fontDir.enable = true;
+    packages = with pkgs; [
+      adwaita-fonts
+      fira-code
+      noto-fonts-emoji
+      vistafonts
+    ];
+  };
 
   # Additional home manager settings
   home-manager = {
--- modules/msib450mpro.nix
+++ modules/msib450mpro.nix
@@ -3,10 +3,19 @@ _: {
     environment.etc = {
       "sensors3.conf".text = ''
         chip "nct6795-isa-*"
+          ignore in1
+          ignore in4
+          ignore in5
+          ignore in6
+          ignore in9
+          ignore in10
+          ignore in11
+          ignore in12
+          ignore in13
+          ignore in14
           ignore fan1
           ignore fan4
           ignore fan5
-          ignore in10
           ignore temp4
           ignore temp5
           ignore temp6
@@ -17,6 +26,19 @@ _: {
           ignore temp12
           label fan2 "CPU Fan"
           ignore fan3 #"Case Fan"
+          label in0 "Vcore"
+          label in2 "AVCC"
+          label in3 "+3.3V"
+          label in7 "3VSB"
+          label in8 "Vbat"
+          set in2_min  3.3 * 0.90
+          set in2_max  3.3 * 1.10
+          set in3_min  3.3 * 0.90
+          set in3_max  3.3 * 1.10
+          set in7_min  3.3 * 0.90
+          set in7_max  3.3 * 1.10
+          set in8_min  3.0 * 0.90
+          set in8_max  3.3 * 1.10
         chip "amdgpu-pci-*"
           label fan1 "GPU Fan"
       '';
