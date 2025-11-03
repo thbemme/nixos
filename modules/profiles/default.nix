44e229f6b5bcb134d1127924272d9b434fa33bff
Thomas Bemme <thomas.bemme@gmail.com>
2025-11-03 10:32:49 +0100

    Adding trusted users for remote build

--- modules/profiles/default.nix
+++ modules/profiles/default.nix
@@ -7,6 +7,7 @@
     settings = {
       experimental-features = ["nix-command" "flakes"];
       auto-optimise-store = true;
+      trusted-users = ["@wheel"];
     };
     gc = {
       automatic = true;
