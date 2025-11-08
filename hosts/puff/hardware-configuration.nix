3b128b701f0d603ac71cee0328251298ba6b456a
Thomas Bemme <thomas.bemme@gmail.com>
2025-11-08 17:21:01 +0100

    Cleanup Hardware configurations

--- hosts/puff/hardware-configuration.nix
+++ hosts/puff/hardware-configuration.nix
@@ -7,67 +7,64 @@
 }: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
-    inputs.nixos-hardware.nixosModules.common-pc-laptop
     inputs.nixos-hardware.nixosModules.common-cpu-intel
+    inputs.nixos-hardware.nixosModules.common-pc
+    inputs.nixos-hardware.nixosModules.common-pc-laptop
     inputs.nixos-hardware.nixosModules.common-pc-ssd
   ];
 
-  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usb_storage" "sd_mod"];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-intel"];
-  boot.extraModulePackages = [];
-
-  # Resume for hibernation
-  boot.resumeDevice = "/dev/disk/by-label/rootfs";
-
-  # btrfs inspect-internal map-swapfile -r /var/lib/swapfile
-  boot.kernelParams = ["resume_offset=93038216"];
-
-  boot.initrd.luks.devices."rootfs" = {
-    device = "/dev/disk/by-partlabel/root";
-    bypassWorkqueues = true; # increase SSD performance
-    allowDiscards = true; # allow fstrim; it might reveal information about the filesystem
+  # Boot configuration
+  boot = {
+    initrd = {
+      availableKernelModules = ["xhci_pci" "ahci" "usb_storage" "sd_mod"];
+      kernelModules = [];
+      luks.devices."rootfs" = {
+        device = "/dev/disk/by-partlabel/root";
+        bypassWorkqueues = true; # Improve SSD performance
+        allowDiscards = true; # Enable fstrim (note: potential info leak)
+      };
+    };
+    kernelModules = ["kvm-intel"];
+    extraModulePackages = [];
+    resumeDevice = "/dev/disk/by-label/rootfs";
+    kernelParams = ["resume_offset=93038216"];
   };
 
-  fileSystems."/" = {
-    label = "rootfs";
-    fsType = "btrfs";
-    options = ["subvol=root" "compress=zstd" "noatime"];
+  # Filesystem configuration
+  fileSystems = {
+    "/" = {
+      label = "rootfs";
+      fsType = "btrfs";
+      options = ["subvol=root" "compress=zstd" "noatime"];
+    };
+    "/home" = {
+      label = "rootfs";
+      fsType = "btrfs";
+      options = ["subvol=home" "compress=zstd" "noatime"];
+    };
+    "/nix" = {
+      label = "rootfs";
+      fsType = "btrfs";
+      options = ["subvol=nix" "compress=zstd" "noatime"];
+    };
+    "/boot" = {
+      label = "boot";
+      fsType = "vfat";
+      options = ["fmask=0022" "dmask=0022"];
+    };
   };
 
-  fileSystems."/home" = {
-    label = "rootfs";
-    fsType = "btrfs";
-    options = ["subvol=home" "compress=zstd" "noatime"];
-  };
-
-  fileSystems."/nix" = {
-    label = "rootfs";
-    fsType = "btrfs";
-    options = ["subvol=nix" "compress=zstd" "noatime"];
-  };
-
-  fileSystems."/boot" = {
-    label = "boot";
-    fsType = "vfat";
-    options = ["fmask=0022" "dmask=0022"];
-  };
-
-  # Extra swap file for hibernation
+  # Swap configuration for hibernation
   swapDevices = [
     {
       device = "/var/lib/swapfile";
-      size = 12 * 1024;
+      size = 12 * 1024; # 12 GiB swap file
     }
   ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  # Networking
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp1s0.useDHCP = lib.mkDefault true;
 
+  # Platform
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
