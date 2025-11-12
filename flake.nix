651959e7a70dce68c5661697a3a44320174ea086
Thomas Bemme <thomas.bemme@gmail.com>
2025-11-12 16:41:52 +0000

    Update Flake

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
     };
 
     nix-on-droid = {
-      url = "github:nix-community/nix-on-droid/release-24.05";
+      url = "github:nix-community/nix-on-droid";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
