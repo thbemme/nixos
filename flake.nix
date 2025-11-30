59381e6e939ccbbb322b95a3a23a99c95b8915cd
Thomas Bemme <thomas.bemme@gmail.com>
2025-11-30 19:06:57 +0100

    NixOS 25.11 Release

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     nixos-hardware = {
--- home/apps/ssh.nix
+++ home/apps/ssh.nix
@@ -11,6 +11,9 @@
       "gitlab.com github.com" = {
         user = "git";
       };
+      "test-amd64-vm dev-amd64-vm [127.0.0.1]:2222" = {
+        userKnownHostsFile = "/dev/null";
+      };
     };
   };
 }
