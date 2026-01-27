d00ef18c2de5927a1f833c9faec4b16258f72f69
Thomas Bemme <thomas.bemme@gmail.com>
2026-01-27 23:36:13 +0100

    Minimal niri + dms

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -3,9 +3,10 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../../modules/profiles/default.nix
-    ../../modules/profiles/desktop-extras.nix
-    ../../modules/profiles/desktop-gnome.nix
+    #../../modules/profiles/desktop-extras.nix
+    #../../modules/profiles/desktop-gnome.nix
     ../../modules/profiles/desktop-minimal.nix
+    ../../modules/profiles/desktop-niri.nix
     ../../modules/profiles/home.nix
     ../../modules/profiles/security.nix
     ../../modules/profiles/work.nix
---
+++ modules/profiles/desktop-niri.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  programs.dms-shell = {
+    enable = true;
+    systemd = {
+      enable = true; # Systemd service for auto-start
+      restartIfChanged = true; # Auto-restart dms.service when dank-material-shell changes
+    };
+  };
+  systemd.user.services.niri-flake-polkit.enable = false;
+
+  # Niri system requirements (compositor configured via home-manager)
+  security.polkit.enable = true;
+  programs.dconf.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  hardware.graphics.enable = true;
+  security.pam.services.swaylock = {};
+
+  # greetd display manager
+  services.greetd = {
+    enable = true;
+    settings.default_session = {
+      command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd niri-session";
+      user = "greeter";
+    };
+  };
+  services.displayManager.sddm.enable = lib.mkForce false;
+
+  # XDG portals
+  xdg.portal = {
+    enable = true;
+    extraPortals = [pkgs.xdg-desktop-portal-gnome];
+    config.common.default = "*"; # Use first available portal implementation
+  };
+
+  services.udisks2.enable = true;
+
+  programs.niri = {
+    enable = true;
+  };
+
+  # DMS dependencies
+  services.accounts-daemon.enable = true;
+  services.power-profiles-daemon.enable = true;
+
+  programs.xwayland.enable = true;
+}
