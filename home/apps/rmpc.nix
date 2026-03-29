2ed6b335752a0a8146a82933d7b80e4b222d2fca
Thomas Bemme <thomas.bemme@gmail.com>
2026-03-29 21:36:29 +0200

    Update Flake & Config rmpc

--- home/apps/rmpc.nix
+++ home/apps/rmpc.nix
@@ -1,10 +1,7 @@
 {pkgs, ...}: let
   musicDirectory = "~/Music";
 in {
-  home.packages = with pkgs; [rmpc];
-
   services.mpd-mpris.enable = true;
-
   services.mpd = {
     enable = true;
     inherit musicDirectory;
@@ -18,5 +15,79 @@ in {
 
       auto_update "yes"
     '';
+  };
+
+  programs.rmpc = {
+    enable = true;
+    config = ''
+          #![enable(implicit_some)]
+      #![enable(unwrap_newtypes)]
+      #![enable(unwrap_variant_newtypes)]
+      (
+          album_art: (
+              vertical_align: Top,
+          ),
+          search: (
+              case_sensitive: false,
+              mode: Contains,
+              tags: [
+                  (value: "any",         label: "Any Tag"),
+                  (value: "artist",      label: "Artist"),
+                  (value: "album",       label: "Album"),
+                  (value: "albumartist", label: "Album Artist"),
+                  (value: "title",       label: "Title"),
+              ],
+          ),
+          tabs: [
+              (
+                  name: "Queue",
+                  border_type: None,
+                  pane: Split(
+                      direction: Horizontal,
+                      panes: [
+                          (
+                              size: "30%",
+                              borders: "RIGHT",
+                              pane: Pane(AlbumArt),
+                          ),
+                          (
+                              size: "100%",
+                              pane: Pane(Queue),
+                          )
+                      ],
+                  ),
+              ),
+              (
+                  name: "Directories",
+                  border_type: None,
+                  pane: Pane(Directories),
+              ),
+              (
+                  name: "Artists",
+                  border_type: None,
+                  pane: Pane(Artists),
+              ),
+              (
+                  name: "Album Artists",
+                  border_type: None,
+                  pane: Pane(AlbumArtists),
+              ),
+              (
+                  name: "Albums",
+                  border_type: None,
+                  pane: Pane(Albums),
+              ),
+              (
+                  name: "Playlists",
+                  border_type: None,
+                  pane: Pane(Playlists),
+              ),
+              (
+                  name: "Search",
+                  border_type: None,
+                  pane: Pane(Search),
+              ),
+          ],
+      )'';
   };
 }
