77798800899a798ce50c640e2c90b161b122bdbb
Thomas Bemme <thomas.bemme@bs.nttdata.com>
2026-04-28 07:42:58 +0000

    Update Flake & Move OOM configuration to desktop

--- modules/profiles/default.nix
+++ modules/profiles/default.nix
@@ -80,38 +80,15 @@ in {
     "vm.dirty_ratio" = 20;
   };
 
-  # Services configuration
-  services = {
-    # Kill applications on OOM... prior to the desktop locking up.
-    earlyoom = {
-      enable = true;
-      enableNotifications = true;
-      freeMemThreshold = 20;
-      extraArgs = [
-        # Avoid killing important system and desktop processes
-        "--avoid"
-        "^(systemd|kernel|init|dbus|NetworkManager|pipewire)$"
-        "--avoid"
-        "^(gnome-shell|wayland|niri|dms)$"
-        "--avoid"
-        "^(gdm|sddm|lightdm|greetd)$"
-        # Prefer killing these types of processes first
-        "--prefer"
-        "^(chrome|chromium|firefox|electron)$"
-        "--prefer"
-        "^(java|node|python|ruby)$"
-      ];
-    };
-    # Disk schedulers optimized for different storage types
-    udev.extraRules = ''
-      # Set the 'kyber' I/O scheduler for NVMe SSDs. This is optimized for the
-      # low latency and high parallelism of modern NVMe drives.
-      ACTION=="add|change", KERNEL=="nvme?n?", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="kyber"
+  # Disk schedulers optimized for different storage types
+  services.udev.extraRules = ''
+    # Set the 'kyber' I/O scheduler for NVMe SSDs. This is optimized for the
+    # low latency and high parallelism of modern NVMe drives.
+    ACTION=="add|change", KERNEL=="nvme?n?", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="kyber"
 
-      # Set the 'bfq' I/O scheduler for SATA SSDs and rotational HDDs.
-      # This scheduler is optimized for desktop responsiveness on these device types.
-      ACTION=="add|change", KERNEL=="sd*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="bfq"
-      ACTION=="add|change", KERNEL=="sd*", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="bfq"
-    '';
-  };
+    # Set the 'bfq' I/O scheduler for SATA SSDs and rotational HDDs.
+    # This scheduler is optimized for desktop responsiveness on these device types.
+    ACTION=="add|change", KERNEL=="sd*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="bfq"
+    ACTION=="add|change", KERNEL=="sd*", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="bfq"
+  '';
 }
