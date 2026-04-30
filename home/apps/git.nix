ac5eb06c5f818e6711c7a81ccd35e8e22fd8b090
Thomas Bemme <thomas.bemme@bs.nttdata.com>
2026-04-30 07:41:33 +0000

    Fix credential helper

--- home/apps/git.nix
+++ home/apps/git.nix
@@ -2,15 +2,14 @@
   programs.git = {
     enable = true;
     settings = {
+      credential.helper = "cache --timeout=36000";
+      init.defaultBranch = "main";
+      log.date = "iso";
+      pull.rebase = true;
+      push.autoSetupRemote = true;
       user = {
         name = "${vars.gitName}";
         email = "${vars.gitEmail}";
-      };
-      init.defaultBranch = "main";
-      pull.rebase = true;
-      push.autoSetupRemote = true;
-      extraConfig = {
-        credential.helper = "store";
       };
     };
   };
