c28e07571aeaca07ab1818ee4dbce046ecd75b40
Thomas Bemme <thomas.bemme@gmail.com>
2026-06-27 16:05:16 +0200

    Switching to local Stylix for https://github.com/nix-community/stylix/pull/2378

--- flake.nix
+++ flake.nix
@@ -18,7 +18,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    stylix.url = "github:nix-community/stylix/release-26.05";
+    #stylix.url = "github:nix-community/stylix/release-26.05";
+    stylix.url = "github:thbemme/stylix/dank-calendar";
 
     niri = {
       url = "github:sodiboo/niri-flake";
