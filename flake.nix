34373525937bc30e3c9dc3e08f0634602c03c5d4
Thomas Bemme <thomas.bemme@gmail.com>
2026-06-30 12:01:58 +0200

    Update Flake & revert to official stylix

--- flake.nix
+++ flake.nix
@@ -18,8 +18,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    #stylix.url = "github:nix-community/stylix/release-26.05";
-    stylix.url = "github:thbemme/stylix/dank-calendar";
+    stylix.url = "github:nix-community/stylix/release-26.05";
 
     niri = {
       url = "github:sodiboo/niri-flake";
