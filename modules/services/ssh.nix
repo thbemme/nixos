61a68fa7aa4e448d13d359c3bc95df8728b6ea43
Thomas Bemme <thomas.bemme@bs.nttdata.com>
2026-07-13 08:47:19 +0000

    Update Flake & cleanup ssh config

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -15,6 +15,8 @@ in {
   services.openssh = {
     enable = true;
     settings = {
+      AllowAgentForwarding = false;
+      AllowTcpForwarding = false;
       AllowUsers = [vars.user];
       Banner = toString (
         pkgs.writeText "ssh_banner" ''
@@ -29,52 +31,39 @@ in {
 
         ''
       );
-      PasswordAuthentication = false;
-      PermitEmptyPasswords = false;
-      PermitTunnel = false;
-      UseDns = false;
-      KbdInteractiveAuthentication = false;
-      X11Forwarding = false;
+      ClientAliveCountMax = 0;
+      ClientAliveInterval = 300;
+      KbdInteractiveAuthentication = lib.mkDefault false;
+      LogLevel = "VERBOSE";
       MaxAuthTries = 3;
       MaxSessions = 2;
-      ClientAliveInterval = 300;
-      ClientAliveCountMax = 0;
+      PasswordAuthentication = lib.mkDefault false;
+      PermitEmptyPasswords = false;
+      PermitRootLogin = lib.mkDefault "no";
+      PermitTunnel = false;
       TCPKeepAlive = false;
-      AllowTcpForwarding = false;
-      AllowAgentForwarding = false;
-      LogLevel = "VERBOSE";
-      PermitRootLogin = "no";
+      UseDns = false;
+      X11Forwarding = false;
+
       KexAlgorithms = [
         # Post-Quantum: https://www.openssh.org/pq.html
         "mlkem768x25519-sha256"
-        "sntrup761x25519-sha512"
-        "curve25519-sha256@libssh.org"
-        "ecdh-sha2-nistp521"
-        "ecdh-sha2-nistp384"
-        "ecdh-sha2-nistp256"
-        "diffie-hellman-group-exchange-sha256"
+        "sntrup761x25519-sha512@openssh.com"
       ];
+
       Ciphers = [
         "aes256-gcm@openssh.com"
-        "aes128-gcm@openssh.com"
-        # stream cipher alternative to aes256, proven to be resilient
-        # Very fast on basically anything
         "chacha20-poly1305@openssh.com"
-        # industry standard, fast if you have AES-NI hardware
         "aes256-ctr"
-        "aes192-ctr"
-        "aes128-ctr"
       ];
+
       Macs = [
-        # Combines the SHA-512 hash func with a secret key to create a MAC
         "hmac-sha2-512-etm@openssh.com"
         "hmac-sha2-256-etm@openssh.com"
         "umac-128-etm@openssh.com"
-        "hmac-sha2-512"
-        "hmac-sha2-256"
-        "umac-128@openssh.com"
       ];
     };
+
     # These keys will be generated for you
     hostKeys = [
       {
