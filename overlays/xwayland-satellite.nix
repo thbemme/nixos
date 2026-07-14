88d00a6d3a10faeaee2f029ef9a242babd1a5ca3
Thomas Bemme <thomas.bemme@gmail.com>
2026-07-14 18:12:26 +0200

    Update Flake

--- overlays/xwayland-satellite.nix
+++ overlays/xwayland-satellite.nix
@@ -6,7 +6,7 @@ final: prev: {
         owner = "Supreeeme";
         repo = "xwayland-satellite";
         rev = "master";
-        sha256 = "sha256-28696iIw8uE0ZUyFTtzhEM8xMh85clCYypMxkvUi+sc=";
+        sha256 = "sha256-Dl0Gvrig3EpE962hzF3ETPhUztlfuRhcmlpd8ioHN54=";
       };
 
       # overrideAttrs works on the mkDerivation, so we cannot override cargoHash.
