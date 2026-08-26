e6f7f1492a38d9634ceb5e70c5a380635b55a35f
Thomas Bemme <thomas.bemme@gmail.com>
2026-08-26 17:39:05 +0200

    Update Flake

--- modules/profiles/desktop-extras.nix
+++ modules/profiles/desktop-extras.nix
@@ -1,43 +1,38 @@
 {
   pkgs,
-  pkgs-unstable,
   vars,
   ...
 }: {
-  environment.systemPackages =
-    (with pkgs; [
-      _64gram
-      amberol
-      btrfs-assistant
-      cozy
-      cryptomator
-      ffmpeg-full
-      gedit
-      gimp3-with-plugins
-      handbrake
-      hunspell
-      hunspellDicts.de_DE
-      hunspellDicts.en_US
-      krita
-      libreoffice-fresh
-      lm_sensors
-      loupe
-      lyx
-      nextcloud-client
-      papers
-      pika-backup
-      remmina
-      scribus
-      stellarium
-      tenacity
-      texlive.combined.scheme-small
-      timg
-      tor-browser
-      transmission_4-gtk
-    ])
-    ++ (with pkgs-unstable; [
-      #_64gram
-    ]);
+  environment.systemPackages = with pkgs; [
+    _64gram
+    amberol
+    btrfs-assistant
+    cozy
+    cryptomator
+    ffmpeg-full
+    gedit
+    gimp3-with-plugins
+    handbrake
+    hunspell
+    hunspellDicts.de_DE
+    hunspellDicts.en_US
+    krita
+    libreoffice-fresh
+    lm_sensors
+    loupe
+    lyx
+    nextcloud-client
+    papers
+    pika-backup
+    remmina
+    scribus
+    stellarium
+    tenacity
+    texlive.combined.scheme-small
+    timg
+    tor-browser
+    transmission_4-gtk
+  ];
 
   # Appimage support
   programs.appimage.enable = true;
