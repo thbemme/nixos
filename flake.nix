7166acc8cf142199c4d888ec2d5837970e3847cb
Thomas Bemme <thomas.bemme@bs.nttdata.com>
2026-09-17 08:09:39 +0000

    Switch to nixos.org tarballs

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
   description = "NixOS config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixos-26.05/nixexprs.tar.zst";
+    nixpkgs-unstable.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.zst";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
